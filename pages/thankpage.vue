ab2e5a00000dc6ad6180130c0d27827bbd10e1c5
tieudamtac <hieungochoangit@gmail.com>
2021-04-05 14:05:54 +0700

    thank page

---
+++ pages/thankpage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="thank-title">ありがとう</h1>
+            </div>
+            <NuxtLink to="/">ホームページ</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .thank-title {
+        font-size: 120px;
+        text-align: center;
+        padding: 40px;
+    }
+</style>
